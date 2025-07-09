ebcd17717c4c899f405d13c65db27bf17381f77d
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-07-09 02:52:27 -0300

    add auth/users module and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TarefasModule } from './tarefas/tarefas.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { TarefasModule } from './tarefas/tarefas.module';
 
 @Module({
-  imports: [TarefasModule],
+  imports: [TarefasModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
