3304a49c59b5de549cf4726683b34fbc241c871e
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-07-10 20:47:14 -0300

    ajusta cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+    origin: 'http://localhost:*', // somente para DEV
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
