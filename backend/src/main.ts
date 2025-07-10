b462a7fe8a8dd60f3cad4b947544f4acad321ac0
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-07-10 13:00:22 -0300

    atualizacao swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('API do NestJS')
+    .setDescription('Documentação da API NestJS')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
