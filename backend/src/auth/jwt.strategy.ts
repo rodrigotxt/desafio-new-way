63369c2ac1bd4c47c9057dec8de8dbb112333502
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-07-10 03:57:18 -0300

    adiciona jwt strategy para validacao de token jwt

---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+// jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new Error('JWT Secret deve estar definido em jwtConstants.secret');
+    }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username };
+  }
+}
