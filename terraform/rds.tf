90d36bc21076d9b7a360ce4cb3183e20c54bffaa
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-07-11 17:27:45 -0300

    corrige versao banco e inclui makefile

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -37,7 +37,7 @@ resource "aws_db_subnet_group" "main" {
 resource "aws_db_instance" "main" {
   allocated_storage    = 20
   engine               = "postgres"
-  engine_version       = "16.1"
+  engine_version       = "16"
   instance_class       = "db.t3.micro"
   db_name              = var.db_name
   username             = var.db_user
